0679f461ffee15ed47a4d5d16b182309ed11a0bd
abiola oladunjoye <abiola.oladunjoye@tm30.net>
2023-05-28 16:15:31 +0100

    helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "i love machine learning" << '\n';
+    // use endl due to it clear the buffer and \n givbe better performance
+    cout << "nice result in seconds" << endl;
+    return 0;
+}
